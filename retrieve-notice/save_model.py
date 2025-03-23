8c0bca64c7496727c59f4e764c4c23c1175add97
denev6 <sungjin.code@gmail.com>
2025-03-23 15:57:16 +0900

    move retrieve-notice to a separate repo

--- retrieve-notice/save_model.py
+++
@@ -1,6 +0,0 @@
-import torch
-from sentence_transformers import SentenceTransformer
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = SentenceTransformer("snunlp/KR-SBERT-V40K-klueNLI-augSTS", device=device)
-model.save("snunlp/KR-SBERT-V40K-klueNLI-augSTS")
